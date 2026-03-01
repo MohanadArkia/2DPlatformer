1adceb3bbcca1b72a5abb9211f81ff0dd0e634e0
Mohanad <none@example.com>
2026-03-01 05:36:40 +0200

    player.c refactor

--- src/player.c
+++ src/player.c
@@ -1,6 +1,116 @@
 #include "player.h"
 #include "game.h"
 
+// TODO: Refactor
+bool CheckCollisionPlayerObject(Player* player, GameObject* obj)
+{
+    float playerLeft   = player->position.x - player->radius;
+    float playerRight  = player->position.x + player->radius;
+    float playerTop    = player->position.y - player->radius;
+    float playerBottom = player->position.y + player->radius;
+
+    float objLeft   = obj->position.x;
+    float objRight  = obj->position.x + obj->size.x;
+    float objTop    = obj->position.y;
+    float objBottom = obj->position.y + obj->size.y;
+
+    bool overlapX = playerRight > objLeft && playerLeft < objRight;
+    bool overlapY = playerBottom > objTop && playerTop < objBottom;
+
+    return overlapX && overlapY;
+}
+
+void HandleGroundCollision(Player* player, GameObject* obj)
+{
+    if (!CheckCollisionPlayerObject(player, obj))
+    {
+	return;
+    }
+    if (player->velocity.y >= 0)
+    {
+	player->position.y = obj->position.y - player->radius;
+	player->velocity.y = 0;
+	player->isGrounded = true;
+    }
+}
+
+void HandleCollectableCollision(Player* player, GameObject* obj)
+{
+    if (!CheckCollisionPlayerObject(player, obj))
+    {
+	return;
+    }
+
+    obj->active = false;
+    player->score += 1;
+    player->isCollected = true;
+}
+
+// TODO: Refactor
+void HandlePlatformCollision(Player *player, GameObject *obj)
+{
+    if (!CheckCollisionPlayerObject(player, obj))
+    {
+	return;
+    }
+
+    float playerLeft   = player->position.x - player->radius;
+    float playerRight  = player->position.x + player->radius;
+    float playerTop    = player->position.y - player->radius;
+    float playerBottom = player->position.y + player->radius;
+
+    float objLeft   = obj->position.x;
+    float objRight  = obj->position.x + obj->size.x;
+    float objTop    = obj->position.y;
+    float objBottom = obj->position.y + obj->size.y;
+
+    float overlapLeft   = playerRight - objLeft;
+    float overlapRight  = objRight - playerLeft;
+    float overlapTop    = playerBottom - objTop;
+    float overlapBottom = objBottom - playerTop;
+
+    float minX = (overlapLeft < overlapRight) ? overlapLeft : overlapRight;
+    float minY = (overlapTop < overlapBottom) ? overlapTop : overlapBottom;
+
+    if (minX < minY)
+    {
+        if (overlapLeft < overlapRight)
+	{
+            player->position.x -= overlapLeft;
+	}
+        else
+	{
+            player->position.x += overlapRight;
+	}
+        player->velocity.x = 0;
+    }
+    else
+    {
+        if (overlapTop < overlapBottom)
+        {
+            player->position.y -= overlapTop;
+            player->isGrounded = true;
+        }
+        else
+        {
+            player->position.y += overlapBottom;
+        }
+
+        player->velocity.y = 0;
+    }
+}
+
+void HandleEnemyCollision(Player *player, GameObject *obj)
+{
+    if (!CheckCollisionPlayerObject(player, obj))
+    {
+	return;
+    }
+
+    player->position.x = 0;
+    player->position.y = 350;
+    player->velocity = (Vector2){0, 0};
+}
 
 void Player_Init(Player *player, float x, float y)
 {
@@ -14,6 +124,7 @@ void Player_Init(Player *player, float x, float y)
     player->isCollected = false;
 }
 
+// TODO: Refactor
 bool Player_Update(Player *player, float deltaTime, GameObject *objects, int objectCount)
 {
     const float gravity = 900.0f;
@@ -57,137 +168,34 @@ bool Player_Update(Player *player, float deltaTime, GameObject *objects, int obj
     player->isGrounded = false;
     for (int i = 0; i < objectCount; i++)
     {
-        GameObject *obj = &objects[i];
-        if (!obj->active)
-        {
-            continue;
-        }
-
-        if (obj->tag == TAG_GROUND)
-        {
-            float playerBottom = player->position.y + player->radius;
-            float playerLeft = player->position.x - player->radius;
-            float playerRight = player->position.x + player->radius;
-            float objTop = obj->position.y;
-            float objLeft = obj->position.x;
-            float objRight = obj->position.x + obj->size.x;
-
-            bool collidingX = playerRight > objLeft && playerLeft < objRight;
-            bool collidingY = playerBottom > objTop && player->position.y < objTop + obj->size.y;
-
-            if (obj->tag == TAG_GROUND && collidingX && collidingY && player->velocity.y >= 0)
-            {
-                // Snap player to top of object
-                player->position.y = objTop - player->radius;
-                player->velocity.y = 0;
-                player->isGrounded = true;
-            }
-        }
-
-        if (obj->tag == TAG_COLLECTABLE)
-        {
-            float playerLeft = player->position.x - player->radius;
-            float playerRight = player->position.x + player->radius;
-            float playerTop = player->position.y - player->radius;
-            float playerBottom = player->position.y + player->radius;
-
-            float objLeft = obj->position.x;
-            float objRight = obj->position.x + obj->size.x;
-            float objTop = obj->position.y;
-            float objBottom = obj->position.y + obj->size.y;
-
-            bool collidingX = playerRight > objLeft && playerLeft < objRight;
-            bool collidingY = playerBottom > objTop && playerTop < objBottom;
-
-            if (collidingX && collidingY)
-            {
-                obj->active = false;
-                player->score += 1;
-                player->isCollected = true;
-            }
-        }
-
-        if (obj->tag == TAG_PLATFORM)
-        {
-            float playerLeft = player->position.x - player->radius;
-            float playerRight = player->position.x + player->radius;
-            float playerTop = player->position.y - player->radius;
-            float playerBottom = player->position.y + player->radius;
-
-            float objLeft = obj->position.x;
-            float objRight = obj->position.x + obj->size.x;
-            float objTop = obj->position.y;
-            float objBottom = obj->position.y + obj->size.y;
-
-            bool overlapX = playerRight > objLeft && playerLeft < objRight;
-            bool overlapY = playerBottom > objTop && playerTop < objBottom;
-
-            if (overlapX && overlapY)
-            {
-                float overlapLeft = playerRight - objLeft;
-                float overlapRight = objRight - playerLeft;
-                float overlapTop = playerBottom - objTop;
-                float overlapBottom = objBottom - playerTop;
-
-                float minX = (overlapLeft < overlapRight) ? overlapLeft : overlapRight;
-                float minY = (overlapTop < overlapBottom) ? overlapTop : overlapBottom;
-
-                if (minX < minY)
-                {
-                    if (overlapLeft < overlapRight)
-                    {
-                        player->position.x -= overlapLeft;
-                    }
-                    else
-                    {
-                        player->position.x += overlapRight;
-                    }
-                    player->velocity.x = 0;
-                }
-                else
-                {
-                    if (overlapTop < overlapBottom)
-                    {
-                        player->position.y -= overlapTop;
-                        player->isGrounded = true;
-                    }
-                    else
-                    {
-                        player->position.y += overlapBottom;
-                    }
-                    player->velocity.y = 0;
-                }
-            }
-        }
-
-	if (obj->tag == TAG_ENEMY)
+	GameObject *obj = &objects[i];
+	if (!obj->active)
 	{
-            float playerLeft = player->position.x - player->radius;
-            float playerRight = player->position.x + player->radius;
-            float playerTop = player->position.y - player->radius;
-            float playerBottom = player->position.y + player->radius;
-
-            float objLeft = obj->position.x;
-            float objRight = obj->position.x + obj->size.x;
-            float objTop = obj->position.y;
-            float objBottom = obj->position.y + obj->size.y;
-
-            bool collidingX = playerRight > objLeft && playerLeft < objRight;
-            bool collidingY = playerBottom > objTop && playerTop < objBottom;
-
-            if (collidingX && collidingY)
-            {
-		player->position.x = 0;
-		player->position.y = 350;
-            }
+	    continue;
 	}
+
+	switch(obj->tag)
+	{
+	    case TAG_GROUND:
+		HandleGroundCollision(player, obj);
+		break;
+	    case TAG_COLLECTABLE:
+		HandleCollectableCollision(player, obj);
+		break;
+	    case TAG_PLATFORM:
+		HandlePlatformCollision(player, obj);
+		break;
+	    case TAG_ENEMY:
+		HandleEnemyCollision(player, obj);
+		break;
+	    }
+	}
+	if (player->score == 3)
+	{
+	    DrawText("YOU WON!!", 130, 200, 100, RED);
+	}
+	return jumped;
     }
-    if (player->score == 3)
-    {
-	DrawText("YOU WON!!", 130, 200, 100, RED);
-    }
-    return jumped;
-}
 
 void Player_Draw(Player *player)
 {
